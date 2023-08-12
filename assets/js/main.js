ce2be397a67ef4e50017c2ca30b46ec6e906dd1a
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-08-12 16:06:15 +0300

    Added responsiveness to navbar. Also added slide animation for navbar.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
+// toggle responsive menu
+const toggleMenuBtn = document.querySelector('#toggleBtn');
+const navLinks = document.querySelector('#navLinksRow');
+const joinBtn = document.querySelector('#joinBtn');
 
+toggleMenuBtn.addEventListener('click', toggleMenu);
+joinBtn.addEventListener('click', removeActiveClass);
+navLinks.addEventListener('click', (e) => {
+  if (e.target.tagName === 'A') {
+    removeActiveClass();
+  }
+});
+
+function toggleMenu() {
+  navLinks.classList.toggle('active');
+}
+
+function removeActiveClass() {
+  navLinks.classList.remove('active');
+}
